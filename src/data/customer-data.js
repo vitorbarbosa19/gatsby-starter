1c8f5b15b208eda9f176315731b72ccdd979d3e5
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-12-21 16:51:51 -0800

    start making changes to boilerplate

--- src/data/customer-data.js
+++ src/data/customer-data.js
@@ -1,26 +1,26 @@
 export default [
   {
-    title: 'Lorem ipsum dolor sit amet, consectetur adipiscing',
+    title: 'Ganhe tempo',
     content:
-      'Eu lobortis elementum nibh tellus molestie nunc non blandit massa. Sit amet consectetur adipiscing elit duis.',
-    customerName: 'Jane Doe',
-    customerTitle: 'Director of Research and Data',
+      'Não me preocupo em despachar ou acompanhar volumes, não fica mercadoria para trás e tenho tempo para ir em mais lojas.',
+    customerName: 'Giullia Beatriz',
+    customerTitle: 'Cliente desde 2016',
     customerImage: 'https://placeimg.com/150/150/people'
   },
   {
-    title: 'Lorem ipsum dolor sit amet, consectetur adipiscing',
+    title: 'Ganhe diversidade',
     content:
-      'Eu lobortis elementum nibh tellus molestie nunc non blandit massa. Sit amet consectetur adipiscing elit duis.',
-    customerName: 'John Doe',
-    customerTitle: 'Director of Research and Data',
+      'Posso experimentar novas marcas à distância sem lidar com a burocracia. Hoje tenho mais diversidade do que antes, mesmo viajando menos.',
+    customerName: 'Udes Regina',
+    customerTitle: 'Cliente desde 2014',
     customerImage: 'https://placeimg.com/150/150/people'
   },
   {
-    title: 'Lorem ipsum dolor sit amet, consectetur adipiscing',
+    title: 'Ganhe segurança',
     content:
-      'Eu lobortis elementum nibh tellus molestie nunc non blandit massa. Sit amet consectetur adipiscing elit duis.',
-    customerName: 'Jane Smith',
-    customerTitle: 'Director of Research and Data',
+      'Tem fabricante que pede os dados do meu cartão para que eu pague à distância. Com a Ziro eu pago via app e tenho mais segurança.',
+    customerName: 'Diego de Oliveira',
+    customerTitle: 'Cliente desde 2019',
     customerImage: 'https://placeimg.com/150/150/people'
   }
 ];
